5eb9530280e75f3dea414b75c396b59b01328483
Bushogun <sotto_j@hotmail.com>
2023-09-03 22:53:15 -0500

    style: :lipstick: deploy hardcoded items

--- app/src/components/list-items/item/product-item.tsx
+++ app/src/components/list-items/item/product-item.tsx
@@ -1,39 +1,34 @@
 import React from 'react';
 import styles from './product-item.module.scss'
-// import { Product } from '@/interfaces/ProductItem'
 
-// interface ProductItemProps {
-//   product: Product; 
-// }
-
-// const ProductItem: React.FC<ProductItemProps> = ({ product }) => {
-//   return (
-//     <div>
-//       <img src={product.picture} alt={product.title} />
-//       <h3>{product.title}</h3>
-//       <p>{product.price.currency} {product.price.amount}</p>
-//       <p>{product.address.state_name}, {product.address.city_name}</p>
-//     </div>
-//   );
-// };
 
 export const ProductItem = () => {
     return (
-      <div className={styles.productContainer}>
-        <img src="https://http2.mlstatic.com/D_619667-MLA47781882790_102021-I.jpg"
-        alt="Apple iPhone 13 (128 Gb) - Azul" />
-        754000
-        <h3>Apple iPhone 13 (128 Gb) - Azul</h3>
-        <p>"quantity": 12,</p>
-        <p>"amount": 141752,</p>
-        <p>
-        "state_name": "Capital Federal",
-        "city_name": "Colegiales"
-        </p>
-        <p>"condition": "new",
-        free_shipping": true,
-        </p>
-        <hr />
+      <div className={styles.product_container}>
+        <div className={styles.info_container}>
+          <div className={styles.container_sell_info}>
+            <img src="https://http2.mlstatic.com/D_619667-MLA47781882790_102021-I.jpg"
+            alt="image product  Apple iPhone 13 (128 Gb) - Azul" width="180" height="180" />
+            <div className={styles.container_state_info}>
+              <p className={styles.condition}>new</p>
+              <p className={styles.free_shipping}>true</p>
+              {/* TbTruckDelivery */}
+            </div>
+          </div>
+          <div className={styles.product_description}>
+            <h1 className={styles.title_price}>$754000</h1>
+            <h2 className={styles.title}>Apple iPhone 13 (128 Gb) - Azul</h2>
+            <p className={styles.quantity}> En 12 cuotas de $141752</p>
+            {/* <p className={styles.amount}>Cuotas desde: $ 141752</p> */}
+          </div>  
+
+          <div className={styles.container_details}>
+            <p className={styles.state_name}>Capital Federal</p>
+            <p className={styles.city_name}>Colegiales</p>
+          </div>
+          
+        </div>
+          <hr />
       </div>
     );
   };
